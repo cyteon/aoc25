c41b1e2c6c651ae8ddd079e98733beb2258ab372
Cyteon <129582290+cyteon@users.noreply.github.com>
2025-12-08 07:58:09 +0000

    day 4 part 1

--- day-4/part-1.py
+++ day-4/part-1.py
@@ -12,10 +12,44 @@ with open("day-4/input.txt", "r") as file:
         
         map.append(section)
 
-for y in map:
-    for x in y:
-        pass
+for iy, y in enumerate(map):
+    for ix, x in enumerate(y):
+        if x == "@":
+            adjacent = 0
 
-print(map)
+            if iy > 0:
+                if map[iy - 1][ix] == "@":
+                    adjacent += 1
+                
+                if ix > 0:
+                    if map[iy - 1][ix - 1] == "@":
+                        adjacent += 1
+                
+                if ix < len(map[iy - 1]) - 1:
+                    if map[iy - 1][ix + 1] == "@":
+                        adjacent += 1
+            
+            if ix > 0:
+                if map[iy][ix - 1] == "@":
+                    adjacent += 1
+            
+            if ix < len(map[iy]) - 1:
+                if map[iy][ix + 1] == "@":
+                    adjacent += 1
+            
+            if iy < len(map) - 1:
+                if map[iy + 1][ix] == "@":
+                    adjacent += 1
+                
+                if ix > 0:
+                    if map[iy + 1][ix - 1] == "@":
+                        adjacent += 1
+                
+                if ix < len(map[iy + 1]) - 1:
+                    if map[iy + 1][ix + 1] == "@":
+                        adjacent += 1
+            
+            if adjacent < 4:
+                amount_can_take += 1
 
-# havent finished day 4 yet
+print(amount_can_take)

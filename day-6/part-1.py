1b90d2396ba29c50e41a0d530ab917adc758b3ea
Cyteon <129582290+cyteon@users.noreply.github.com>
2025-12-20 09:52:03 +0000

    day 6

--- day-6/part-1.py
+++ day-6/part-1.py
@@ -2,8 +2,23 @@ total = 0
 
 with open("day-6/input.txt", "r") as file:
     content = file.read()
+    lines = content.split("\n")
 
+    rows = []
+
+    for i in range(4):
+        rows.append([int(x) for x in lines[i].split()])
     
-    
+    ops = lines[4].split()
+
+    for i in range(len(rows[0])):
+        result = 0
+
+        if ops[i] == "+":
+            result = rows[0][i] + rows[1][i] + rows[2][i] + rows[3][i]
+        else:
+            result = rows[0][i] * rows[1][i] * rows[2][i] * rows[3][i]
+        
+        total += result
 
 print(total)
